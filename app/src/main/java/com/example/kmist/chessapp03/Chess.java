1c06b7cebf710dd0a64c393f4abb0759464b94af
Krishna Mistry <kmistry217@gmail.com>
2018-11-28 13:09:21 -0500

    initial commit

---
+++ app/src/main/java/com/example/kmist/chessapp03/Chess.java
@@ -0,0 +1,13 @@
+package com.example.kmist.chessapp03;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Chess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chess);
+    }
+}
