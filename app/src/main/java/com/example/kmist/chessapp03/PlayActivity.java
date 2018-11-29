46e13b3fe63f75220c612908a999a440c88faf08
Michael Chapman <mjc578@gmail.com>
2018-11-29 12:30:31 -0500

    added back button on play screen

--- app/src/main/java/com/example/kmist/chessapp03/PlayActivity.java
+++ app/src/main/java/com/example/kmist/chessapp03/PlayActivity.java
@@ -1,7 +1,13 @@
 package com.example.kmist.chessapp03;
 
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -24,6 +30,10 @@ public class PlayActivity extends AppCompatActivity implements Serializable {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
+        //make a back button
+        android.support.v7.app.ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
         setUndoListener();
 
     }
@@ -40,10 +50,34 @@ public class PlayActivity extends AppCompatActivity implements Serializable {
                 }
                 else{
                     //do the undoing...
-
+                    undoPressed = true;
                 }
             }
         });
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        DialogInterface.OnClickListener dialogListener = new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                switch (which){
+                    case DialogInterface.BUTTON_POSITIVE:
+                        //Yes button clicked
+                        Intent i = new Intent(PlayActivity.this, Chess.class);
+                        startActivity(i);
+                        break;
+
+                    case DialogInterface.BUTTON_NEGATIVE:
+                        //No button clicked
+                        break;
+                }
+            }
+        };
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(PlayActivity.this);
+        builder.setMessage("Return to main menu? (Game will not be saved)").setPositiveButton("Yes", dialogListener)
+                .setNegativeButton("No", dialogListener).show();
+        return true;
     }
 
 
